ff3586ac074eed390a72ffe49a3371e429b31198
Ryan Neff <raneff@gmail.com>
2016-05-07 19:28:47 -0400

    Audio uploads

--- alexaapp/views.py
+++ alexaapp/views.py
@@ -1,4 +1,6 @@
 from alexaapp import *
+from alexaapp.forms import *
+from flask import flash
 
 @app.route("/", methods=['GET'])
 def index():
@@ -7,3 +9,16 @@ def index():
 @app.route('/assets/<path:path>')
 def send_js(path):
     return send_from_directory('../assets', path)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def add_audio():
+	audio_dirname = '../assets/audio/'
+	form = AudioFileForm(request.form)
+	if request.method == 'POST' and form.validate():
+		form.audio_file.data = request.files['audio_file']
+		if form.audio_file.data:
+			form.audio_file.data.save(audio_dirname + request.form['audio_file_name'])
+			flash('Audiofile added.')
+		else:
+			flash('Error: No audiofile uploaded.')
+	return render_template("form.html", action="Add an audiofile", form=form)
